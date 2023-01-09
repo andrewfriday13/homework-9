be3bec6841b35b99111257678b6f5e24044fb40e
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-01-09 17:52:43 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,22 @@
 // Описаний в документації
 import flatpickr from "flatpickr";
+import Notiflix from 'notiflix';
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
 const startTimer = document.querySelector('button[type="button"]')
+const choiseDate = document.querySelector('input[type="text"]')
 startTimer.addEventListener('click', timer)
 
-  startTimer.disabled = true
+const day = document.querySelector('span[data-days]')
+const hour = document.querySelector('span[data-hours]')
+const minute = document.querySelector('span[data-minutes]')
+const second = document.querySelector('span[data-seconds]')
+
+ startTimer.disabled = true
 
 
+let selectDate 
 
 const options = {
   enableTime: true,
@@ -16,46 +24,61 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
+    if (selectedDates[0] < Date.now()) {
       startTimer.disabled = true
-      alert("бери десь в майбутньому")
+      Notiflix.Notify.failure("бери десь в майбутньому")
     } else {
-      console.log(selectedDates[0].getTime());
       startTimer.disabled = false
+    }    
+    selectDate = selectedDates[0].getTime()
 
-    }
-      return selectedDates
   },
-
 };
 
 
-flatpickr('#datetime-picker',options)     
 
-function timer(evt) {
-  setInterval(() => {
-    console.log('hello')
-  },1000)
+flatpickr('#datetime-picker', options)
 
+function timer() {
+  const timerId = setInterval(() => {
+  
+    startTimer.disabled = true
+    const dataTime = selectDate - Date.now()
+    const { days, hours, minutes, seconds } = convertMs(dataTime)
+
+    day.textContent = days
+    hour.textContent = hours
+    minute.textContent = minutes
+    second.textContent = seconds
+
+    if (selectDate <= 1000) {
+      clearInterval(timerId)
+    startTimer.disabled = false
+
+    }
+  }, 1000)
 }
 
 
+function addZero(value) {
+  return String(value).padStart(2, '0')
+}
+
  function convertMs(ms) {
-  // Number of milliseconds per unit of time
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  const days = addZero(Math.floor(ms / day));
+
+  const hours = addZero(Math.floor((ms % day) / hour));
+
+  const minutes = addZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-
